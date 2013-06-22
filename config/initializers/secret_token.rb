05efe02be275e5ef3d09ab4a5a8ad981f87b005b
Eliran Ben-Zikri <eliranbz@gmail.com>
2013-06-22 22:27:05 +0300

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EToroIT::Application.config.secret_token = '74c2ab1f2c0cccc57beba1e92e146f13d510fd9b166636fb048e7743578456a57c7dec19a35540d34a12de0706fcd7ca5e277bcdada45525db3ab0402b7d617b'
